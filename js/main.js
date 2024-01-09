19769177685fafde0efa136c07b80b5d8d7d60b3
Tam-Weibel <tamaraweibel.comgit config --global user.email tamaraweibel@gmail.com>
2024-01-09 20:01:44 +0100

    html and css

---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const userOption = document.querySelector('.selector');
+const btn = document.querySelector('.btn');
+
+const player = document.querySelector('.player');
+const computer = document.querySelector('.computer');
+
+function handleClick(){
+
+}
+
+btn.addEventListener('click', handleClick);
